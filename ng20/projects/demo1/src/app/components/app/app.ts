f978db139f68c14c858820fc2a1bd2cdbf19146e
Alejandro Cerezo <alce65@hotmail.es>
2025-10-06 14:08:09 +0200

    Relocate App component

--- ng20/projects/demo1/src/app/components/app/app.ts
+++ ng20/projects/demo1/src/app/components/app/app.ts
@@ -5,12 +5,14 @@ import { RouterOutlet } from '@angular/router';
   selector: 'ind-root',
   imports: [RouterOutlet],
   template: `
-    <h1>Welcome to {{ title() }}!</h1>
+    <h1>{{ title() }}</h1>
+    <p>Demo Angular 20 Zoneless</p>
+    <p>Curso de Angular para Indra</p>
 
     <router-outlet />
   `,
   styles: [],
 })
 export class App {
-  protected readonly title = signal('demo1');
+  protected readonly title = signal('Demo1');
 }
