7b116bf1bb567d746a531afd520db3cf6110bc5d
Alejandro Cerezo <alce65@hotmail.es>
2025-10-07 15:17:28 +0200

    Use pipes and configure localization as ES

--- ng20/projects/demo2-pages/src/app/app.config.ts
+++ ng20/projects/demo2-pages/src/app/app.config.ts
@@ -1,5 +1,9 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import { ApplicationConfig, LOCALE_ID, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import es from "@angular/common/locales/es";
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData(es);
 
 import { routes } from './app.routes';
 
@@ -7,6 +11,7 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes)
+    provideRouter(routes),
+    { provide: LOCALE_ID, useValue: 'es' }
   ]
 };
--- ng20/projects/demo2-pages/src/app/core/components/footer/footer.ts
+++ ng20/projects/demo2-pages/src/app/core/components/footer/footer.ts
@@ -1,19 +1,17 @@
-
-import { Component } from '@angular/core';
+import { DatePipe, TitleCasePipe } from '@angular/common';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'ind-footer',
-  imports: [],
+  imports: [DatePipe, TitleCasePipe],
   template: `
     <footer>
       <address>Indra Course</address>
-
+      <p>{{ today() | date:'fullDate' | titlecase }}</p>
     </footer>
   `,
   styles: ``
 })
 export class Footer {
-
-
-
+  today = signal(new Date())
 }
