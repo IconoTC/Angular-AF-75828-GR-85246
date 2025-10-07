85a6651dfd61c8f7bdfbc944bdb6c72bdd18bdbf
Alejandro Cerezo <alce65@hotmail.es>
2025-10-07 10:34:48 +0200

    Add asynchronous counter with signals

--- ng20/projects/demo1/src/app/components/app/app.ts
+++ ng20/projects/demo1/src/app/components/app/app.ts
@@ -1,15 +1,17 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Sample } from "../sample/sample";
+import { Counter } from "../counter/counter";
 
 @Component({
   selector: 'ind-root',
-  imports: [RouterOutlet, Sample],
+  imports: [RouterOutlet, Sample, Counter],
   template: `
     <h1>{{ title() }}</h1>
     <p>Demo Angular 20 Zoneless</p>
     <p>Curso de Angular para Indra</p>
-    <ind-sample></ind-sample>
+    <ind-sample />
+    <ind-counter />
 
     <router-outlet />
   `,
