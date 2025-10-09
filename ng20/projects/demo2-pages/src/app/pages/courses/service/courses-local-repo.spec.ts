49c03d23203270db03d9d7adfd0acd0ffbb84bcf
Alejandro Cerezo <alce65@hotmail.es>
2025-10-09 16:32:19 +0200

    Add zoneless to new tests

--- ng20/projects/demo2-pages/src/app/pages/courses/service/courses-local-repo.spec.ts
+++ ng20/projects/demo2-pages/src/app/pages/courses/service/courses-local-repo.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
 import { CoursesLocalRepo } from './courses-local-repo';
+import { provideZonelessChangeDetection } from '@angular/core';
 
 describe('CoursesLocalRepo', () => {
   let service: CoursesLocalRepo;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [provideZonelessChangeDetection()],
+    });
     service = TestBed.inject(CoursesLocalRepo);
   });
 
