9b5aaf02c26b8b26efb645f1f1978bfe2e406641
Alejandro Cerezo <alce65@hotmail.es>
2025-10-10 11:04:58 +0200

    Configure httpClient and environments

--- ng20/projects/demo2-pages/src/app/app.config.ts
+++ ng20/projects/demo2-pages/src/app/app.config.ts
@@ -6,12 +6,14 @@ import { registerLocaleData } from '@angular/common';
 registerLocaleData(es);
 
 import { routes } from './app.routes';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
     provideRouter(routes),
+    provideHttpClient(withFetch()),
     { provide: LOCALE_ID, useValue: 'es' },
     // {provide: DateService, useFactory: () => new DateService('dd/MM/yyyy') }
     // {provide: DateService, useExisting: DateService }
