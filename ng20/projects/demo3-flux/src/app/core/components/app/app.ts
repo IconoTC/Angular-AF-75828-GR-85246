3dd5ff6ee4eab0619a4264819a9589561889434c
Alejandro Cerezo <alce65@hotmail.es>
2025-10-10 17:46:58 +0200

    Add injection of a value

--- ng20/projects/demo3-flux/src/app/core/components/app/app.ts
+++ ng20/projects/demo3-flux/src/app/core/components/app/app.ts
@@ -1,14 +1,18 @@
-import { Component, inject, OnInit, signal, WritableSignal } from '@angular/core';
+import { Component, inject, InjectionToken, OnInit, signal, WritableSignal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Layout } from "../layout/layout";
 import { TasksStore } from '../../../pages/tasks/services/tasks-store';
 
-
+export const APP_TITLE = new InjectionToken<WritableSignal<string>>('App Title');
 
 @Component({
   selector: 'ind-root',
   imports: [RouterOutlet, Layout],
-
+  providers: [
+    {
+      provide: APP_TITLE, useValue: signal('Demo3 - Pages Flux')
+    }
+  ],
   template: `
   <ind-layout [appTitle]="title()">
     <router-outlet />
--- ng20/projects/demo3-flux/src/app/core/components/header/header.ts
+++ ng20/projects/demo3-flux/src/app/core/components/header/header.ts
@@ -1,11 +1,12 @@
-import { Component, input  } from '@angular/core';
+import { Component, inject, input  } from '@angular/core';
+import { APP_TITLE } from '../app/app';
 
 @Component({
   selector: 'ind-header',
   imports: [],
   template: `
     <header>
-      <h1>{{ appTitle() }}</h1>
+      <h1>{{ appTitle2() }}</h1>
       <p>Demo Angular 20 Zoneless con rutas</p>
       <p>Curso de Angular para Indra</p>
       <ng-content></ng-content>
@@ -18,5 +19,6 @@ export class Header {
   // @Input({
   //   required: true
   // }) appTitle!: WritableSignal<string>;
+  appTitle2 = inject(APP_TITLE);
   appTitle = input.required<string>();
 }
