b8cfdf85dbdae35a4c36b1931dd222f55f0630e4
Alejandro Cerezo <alce65@hotmail.es>
2025-10-10 09:54:49 +0200

    Update contacts form with validations

--- ng20/projects/demo2-pages/src/app/pages/about/components/contact/contact.ts
+++ ng20/projects/demo2-pages/src/app/pages/about/components/contact/contact.ts
@@ -1,6 +1,6 @@
 import { JsonPipe } from '@angular/common';
-import { Component, inject } from '@angular/core';
-import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { Component, inject, OnInit } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'ind-contact',
@@ -13,17 +13,33 @@ import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
         <input type="text" formControlName="name" />
       </label>
 
+        @if (contactFormGroup.get('name')?.invalid && (contactFormGroup.get('name')?.touched)) {
+          <div class="error">El nombre es obligatorio.</div>
+        }
+
       <label>
         <span>Email:</span>
         <input type="email" formControlName="email"  />
       </label>
+
+        @if (contactFormGroup.get('email')?.invalid && (contactFormGroup.get('email')?.touched)) {
+          @if (contactFormGroup.get('email')?.errors?.['required']) {
+            <div class="error">El email es obligatorio.</div>
+          } @else if (contactFormGroup.get('email')?.errors?.['email']) {
+            <div class="error">El email no tiene un formato válido.</div>
+          }
+        }
 
       <label>
         <span>Mensaje:</span>
         <textarea formControlName="message" ></textarea>
       </label>
 
-      <button type="submit">Enviar</button>
+        @if (contactFormGroup.get('message')?.invalid && (contactFormGroup.get('message')?.touched)) {
+          <div class="error">El mensaje es obligatorio.</div>
+        }
+
+      <button type="submit" [disabled]="contactFormGroup.invalid">Enviar</button>
 
       <hr>
       <pre>
@@ -39,15 +55,25 @@ import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
     button {
       margin: 10px 0px;
     }
+    .error {
+      color: red;
+      font-size: 0.8em;
+      margin: 2px 0px 5px 0px;
+    }
   `,
 })
-export class Contact {
+export class Contact implements OnInit {
   fb = inject(FormBuilder);
   contactFormGroup = this.fb.nonNullable.group({
-    name: [''],
-    email: [''],
-    message: [''],
+    name: ['', [Validators.required] ],
+    email: ['', [Validators.required, Validators.email] ],
+    message: ['', [Validators.required]],
   });
+
+  ngOnInit() {
+    console.log('Contact form group', this.contactFormGroup);
+  }
+
 
   handleSubmit() {
     if (this.contactFormGroup.valid) {
